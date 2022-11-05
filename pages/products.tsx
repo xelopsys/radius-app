c023e8b29f5c635bc8119ea560edba2f17e063de
xelopsys <113842385+00013622-wiut@users.noreply.github.com>
2022-11-06 00:29:56 +0500

    some data is changed

--- pages/products.tsx
+++ pages/products.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Products() {
 	return (
 		<main className="ml-[20px] mx-3 md:mr-16 rounded-md w-[1097px] max-w-[1097px] h-[97%] max-h-[875px] flex flex-col justify-center items-center box-border bg-[#F3F5F8]">
-			<div className="w-max h-max">home</div>
+			<div className="w-max h-max">Products</div>
 		</main>
 	);
 }
